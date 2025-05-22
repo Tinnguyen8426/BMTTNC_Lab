d6ffbb13fbd2e4bcf3eafb27bdd9bc9bdbb5b869
Andrew <Andrew@GIG>
2025-05-22 15:35:56 +0700

    [add] lab-02 ex01 ceasar

--- Lab_01/ex01/hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello, World!")
-print("My name is Tin")
-print("HUTECH University")
--- Lab_01/ex02/ex02_01.py
+++
@@ -1,3 +0,0 @@
-ten = input("nhập tên của bạn ")
-tuoi = input("nhập tuổi của bạn ")
-print("chào mừng",ten,"! Bạn",tuoi,"tuổi")
